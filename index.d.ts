885f9a55d876e183064d24bc0ee4ad16468c33e2
Giáp Đông <giapdong992@gmail.com>
2021-01-31 10:17:14 +0700

    Update optional argument for typescript

--- index.d.ts
+++ index.d.ts
@@ -25,18 +25,18 @@ declare interface ResponseData {
 declare type Message = string | null | undefined;
 
 declare class ResFormatter {
-  static create(statusCode: number, error: boolean, message: Message, data: any): ResponseData;
-  static success(message: Message, data: any): ResponseData;
-  static badRequest(message: Message, data: any): ResponseData;
-  static unAuthorized(message: Message, data: any): ResponseData;
-  static forbidden(message: Message, data: any): ResponseData;
-  static notFound(message: Message, data: any): ResponseData;
-  static notAllowed(message: Message, data: any): ResponseData;
-  static requestTimeout(message: Message, data: any): ResponseData;
-  static internalError(message: Message, data: any): ResponseData;
-  static badGateway(message: Message, data: any): ResponseData;
-  static unavailable(message: Message, data: any): ResponseData;
-  static gatewayTimeout(message: Message, data: any): ResponseData;
+  static create(statusCode: number, error?: boolean, message?: Message, data?: any): ResponseData;
+  static success(message?: Message, data?: any): ResponseData;
+  static badRequest(message?: Message, data?: any): ResponseData;
+  static unAuthorized(message?: Message, data?: any): ResponseData;
+  static forbidden(message?: Message, data?: any): ResponseData;
+  static notFound(message?: Message, data?: any): ResponseData;
+  static notAllowed(message?: Message, data?: any): ResponseData;
+  static requestTimeout(message?: Message, data?: any): ResponseData;
+  static internalError(message?: Message, data?: any): ResponseData;
+  static badGateway(message?: Message, data?: any): ResponseData;
+  static unavailable(message?: Message, data?: any): ResponseData;
+  static gatewayTimeout(message?: Message, data?: any): ResponseData;
 }
 
 export = ResFormatter;
